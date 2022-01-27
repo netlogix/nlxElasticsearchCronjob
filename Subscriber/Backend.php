9b7a45b4619b9623a5bc218b0f4ce918f4c3ee14
Vladislav Sultanov <vladislav.sultanov@netlogix.de>
2022-01-27 10:22:44 +0100

    Push attributes changes to Elasticsearch. Code refactoring

--- Subscriber/Backend.php
+++ Subscriber/Backend.php
@@ -15,9 +15,16 @@ use nlxElasticsearchCronjob\Services\Config;
 
 class Backend implements SubscriberInterface
 {
-    private const ENABLED_CATEGORY_ACTIONS = ['addCategoryArticles', 'removeCategoryArticles'];
-    private const ENABLED_MANUAL_SORTING_ACTIONS = ['assignPosition', 'removePosition', 'resetCategory'];
-    private const ENABLED_ARTICLE_ACTIONS = ['setPropertyList', 'saveDetail', 'delete'];
+    private const ENABLED_BACKEND_ACTIONS = [
+        'addCategoryArticles',
+        'removeCategoryArticles',
+        'assignPosition',
+        'removePosition',
+        'resetCategory',
+        'saveDetail',
+        'delete',
+        'saveData',
+    ];
 
     /** @var bool */
     private $isFeatureEnabled;
@@ -40,38 +47,19 @@ class Backend implements SubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_Category' => 'onBackendCategory',
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_ManualSorting' => 'onManualSorting',
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_Article' => 'onBackendArticle',
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_Category' => 'onBackend',
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_ManualSorting' => 'onBackend',
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_Article' => 'onBackend',
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_AttributeData' => 'onBackend',
             'Enlight_Controller_Front_DispatchLoopShutdown' => 'onTerminate',
         ];
     }
 
-    public function onBackendCategory(\Enlight_Controller_ActionEventArgs $args): void
+    public function onBackend(\Enlight_Controller_ActionEventArgs $args): void
     {
         $actionName = $args->getRequest()->getActionName();
 
-        if (false === $this->isFeatureEnabled || false === \in_array($actionName, self::ENABLED_CATEGORY_ACTIONS)) {
-            return;
-        }
-        $this->syncBacklog = true;
-    }
-
-    public function onManualSorting(\Enlight_Controller_ActionEventArgs $args): void
-    {
-        $actionName = $args->getRequest()->getActionName();
-
-        if (false === $this->isFeatureEnabled || false === \in_array($actionName, self::ENABLED_MANUAL_SORTING_ACTIONS)) {
-            return;
-        }
-        $this->syncBacklog = true;
-    }
-
-    public function onBackendArticle(\Enlight_Controller_ActionEventArgs $args): void
-    {
-        $actionName = $args->getRequest()->getActionName();
-
-        if (false === $this->isFeatureEnabled || false === \in_array($actionName, self::ENABLED_ARTICLE_ACTIONS)) {
+        if (false === $this->isFeatureEnabled || false === in_array($actionName, self::ENABLED_BACKEND_ACTIONS)) {
             return;
         }
         $this->syncBacklog = true;
